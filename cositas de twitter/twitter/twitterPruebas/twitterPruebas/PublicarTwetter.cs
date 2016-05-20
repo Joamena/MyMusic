f83617f4bc4c3fbd4818c8477e178df1a7458fd3
giobando <gilberth2005@hotmail.com>
2016-05-20 08:25:49 -0600

    twitter - encriptacion
    Se agrego codigo para publicar a twitter.
    Aun estoy viendo como contar Hashtags.
    Se agrego el codigo para encriptar con sha256

---
+++ cositas de twitter/twitter/twitterPruebas/twitterPruebas/PublicarTwetter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace twitterPruebas
+{
+    public class PublicarTwetter
+    {
+        private readonly OAuthInfo _oauth;
+
+        public PublicarTwetter()
+        {
+            _oauth = new OAuthInfo
+            {
+                AccessToken = "722419719362338816-kndw2f5UZDW8ZevTygrI7o5toRmaK24",
+                AccessSecret = "5csFJywnlDZqBB7l1REXntpAE1fcHDn9cwZLE57RsJpSH",
+                ConsumerKey = "xI3ASM3An2j3SVFwgUUrJuIxj",
+                ConsumerSecret = "BcTgiE43wJy8BERLYjy3H6nO7OnsfJlKH39VjnQHsPkZH19Ryq"
+            };
+        }
+
+        public bool PublicarTweet(string strMensaje)
+        {
+            var twitter = new MetodosTwitter(_oauth);
+            //twitter no acepta repitición de mensajes
+            try
+            {
+                twitter.UpdateStatus(strMensaje);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex);
+                return false;
+            }
+        }
+
+        public bool CountHashtag(string strMensaje)
+        {
+            var twitter = new MetodosTwitter(_oauth);
+            try
+            {
+                twitter.GetMentions();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex);
+                return false;
+            }
+        }
+    }
+}
